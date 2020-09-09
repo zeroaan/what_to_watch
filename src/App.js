8cc0dcf48ad69df515870b95798c94efdb7daffe
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-09-09 22:41:32 +0900

    movie and css

--- src/App.js
+++ src/App.js
@@ -1,92 +1,52 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>{isLoading ? "Loading..." : "We are ready"}</h1>
-      </div>
-    );
-  }
-}
-export default App;
-
-/*
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
 
 export default App;
-*/
-
-/*
-const foodIlike = [
-  {
-    id: 1,
-    name: "potato",
-    rating: 3,
-  },
-  {
-    id: 2,
-    name: "tomato",
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: "banana",
-    rating: 5,
-  },
-];
-
-function Food({ name, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h3>this food is {rating} good!</h3>
-      <hr />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} rating={dish.rating} />;
-}
-
-function App() {
-  return <div>{foodIlike.map(renderFood)}</div>;
-}
-
-export default App;
-*/
